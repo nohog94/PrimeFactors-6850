d273334584f28b0599ca67ed642933b290ee0b73
clock <clockim@naver.com>
2025-06-04 10:54:29 +0900

    First Commit

---
+++ src/test/java/PrimeFactorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class PrimeFactorTest {
+    @Test
+    void PrimeFactorTest(){
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertEquals(10, 10);
+    } {
+    }
+}
