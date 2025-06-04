a962245ecf612fa39926b090d11aef4948a96231
unknown <nohog94@gmail.com>
2025-06-04 10:27:11 +0900

    [feature] 49

--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -37,4 +37,9 @@ class PrimeFactorTest {
     void testPrimefactorOf6() {
         assertEquals(Arrays.asList(2,3), primeFactor.of(6));
     }
+
+    @Test
+    void testPrimefactorOf9() {
+        assertEquals(Arrays.asList(3,3), primeFactor.of(9));
+    }
 }
