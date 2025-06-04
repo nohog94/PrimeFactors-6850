699d1dce127bd16c59f2c1a50259c9dbe0093295
unknown <nohog94@gmail.com>
2025-06-04 10:12:21 +0900

    [feature] 24

--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -1,5 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeFactorTest {
-
+    @Test
+    void testPrimefactorOf1() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertEquals(Arrays.asList(), primeFactor.of(1));
+    }
 }
