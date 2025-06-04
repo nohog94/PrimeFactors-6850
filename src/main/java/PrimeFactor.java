b40fd6ede65ab60b1e2b1af44ff07a2b3a0798fc
Jinchan <zxcvb2023@naver.com>
2025-06-04 10:22:04 +0900

    [feature]29p

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -3,8 +3,10 @@ import java.util.List;
 
 public class PrimeFactor {
 
-    public List<Integer> of(int i) {
+    public List<Integer> of(int number) {
         List<Integer> factor = new ArrayList<>();
+        if (number == 2)
+            factor.add(2);
         return factor;
     }
 }
--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -10,4 +10,10 @@ class PrimeFactorTest {
         PrimeFactor primeFactor = new PrimeFactor();
         assertEquals(Arrays.asList(), primeFactor.of(1));
     }
+
+    @Test
+    void testPrimeFactorOf2() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertEquals(Arrays.asList(2), primeFactor.of(2));
+    }
 }
