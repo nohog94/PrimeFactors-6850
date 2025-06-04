fd0d79a45222915d38369b778e4f1a1e8f71bbaf
Jinchan <zxcvb2023@naver.com>
2025-06-04 10:20:31 +0900

    [feature]24p

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactor {
 
+    public List<Integer> of(int i) {
+        List<Integer> factor = new ArrayList<>();
+        return factor;
+    }
 }
--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -1,5 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeFactorTest {
-
+    @Test
+    void testPrimeFactorOf1() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertEquals(Arrays.asList(), primeFactor.of(1));
+    }
 }
